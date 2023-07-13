169399eba5580329398276a2bdeb5fb21c88b54b
Matheus Marchetti <matheusrmarchetti@gmail.com>
2023-07-13 00:28:49 -0300

    Separated tests
    - Entity creation/destruction test
    - Component assignment/destruction test

--- Canvas/Tests/EntityComponent.h
+++ Canvas/Tests/EntityComponent.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Test.h"
-#include "TestScript.h"
 
 #include "EngineAPI/Components.h"
 #include "EngineAPI/GameEntity.h"
@@ -30,6 +29,7 @@ public:
 	virtual void run() override 
 	{ 
 		basic_creation_test();
+		basic_component_test();
 	}
 
 	virtual void shutdown() override 
@@ -39,6 +39,28 @@ public:
 
 private:
 	void basic_creation_test()
+	{
+		entity::entity e1 = scene::create_entity();
+		id::id_type e1_id = id::index(e1.get_id());
+		id::id_type e1_generation = id::generation(e1.get_id());
+		scene::remove_entity(e1);
+
+		entity::entity e2 = scene::create_entity();
+		id::id_type e2_id = id::index(e2.get_id());
+		id::id_type e2_generation = id::generation(e2.get_id());
+
+		entity::entity e3 = scene::create_entity();
+		id::id_type e3_id = id::index(e3.get_id());
+		id::id_type e3_generation = id::generation(e3.get_id());
+
+		entity::entity e4 = scene::create_entity();
+
+		scene::remove_entity(e2);
+		scene::remove_entity(e3);
+		scene::remove_entity(e4);
+	}
+
+	void basic_component_test()
 	{
 		Random r;
 		r.random = rand() % 20;
@@ -51,6 +73,7 @@ private:
 		r_test.random = r.random * 2;
 
 		r = e1.get_component<Random>();
+		e1.remove_component<Random>();
 
 		scene::remove_entity(e1);
 		entity::entity e2 = scene::create_entity();
@@ -62,36 +85,17 @@ private:
 		id::id_type e3_generation = id::generation(e3.get_id());
 
 		e2.add_component<Random>({ 5 });
-		e2.add_component<TestScript>({ e2 });
+
 		e3.add_component<Random>({ 7 });
-		e3.add_component<TestScript>({ e3 });
 
 		entity::entity e4 = scene::create_entity();
 		e4.add_component<Random>();
-		e4.add_component<TestScript>({ e4 });
 
 		const auto& all_random_components = scene::get_all<Random>();
 
-		// Test for data components
 		for (auto& component : all_random_components)
 		{
 			component.random *= 10;
-		}
-
-		const auto& all_script_components = scene::get_all<TestScript>();
-
-		// Test for script components
-		for (auto& component : all_script_components)
-		{
-			component.create();
-		}
-
-		const auto& all_transform_components = scene::get_all<component::transform>();
-
-		// Test for components added via script
-		for (auto& component : all_transform_components)
-		{
-			std::cout << "Position: [x: " << component.position.x << ", y: " << component.position.y << ", z: " << component.position.z << "]" << std::endl;
 		}
 
 		Random& r1 = e1.get_component<Random>();
